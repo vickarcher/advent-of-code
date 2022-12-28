6f7e63334557db1da38365072cec4e55ed707eae
Ritvik Kumar <legorithu@gmail.com>
2022-12-28 14:16:24 -0500

    updated tabstop to 4 spaces

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,33 +1,33 @@
 # AOC 2015 - Day 1
 
 def get_input(): 
-	return open("day1.in", 'r').read().split('\n')
+    return open("day1.in", 'r').read().split('\n')
 
 def p1():
-	counter = 0
-	
-	for line in get_input():
-		for c in line:
-			if c == "(":
-				counter += 1
-			else:
-				counter -= 1
-	
-	print(counter)
-	
+    counter = 0
+    
+    for line in get_input():
+        for c in line:
+            if c == "(":
+                counter += 1
+            else:
+                counter -= 1
+    
+    print(counter)
+    
 def p2():
-	counter = 0
+    counter = 0
 
-	for line in get_input():
-		for i, c in enumerate(line):
-			if counter == -1:
-				print(i)
-				break
+    for line in get_input():
+        for i, c in enumerate(line):
+            if counter == -1:
+                print(i)
+                break
 
-			if c == "(":
-				counter += 1
-			else:
-				counter -= 1
-	
+            if c == "(":
+                counter += 1
+            else:
+                counter -= 1
+    
 p1()
 p2()
