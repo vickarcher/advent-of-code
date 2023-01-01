90d3955d388a7841e3993876a55da8aadc9104a5
Ritvik Kumar <legorithu@gmail.com>
2023-01-01 09:16:48 -0500

    added 2107 day 2 solution

---
+++ 2017/day2.py
@@ -0,0 +1,36 @@
+# AoC 2017 - Day 2
+
+def get_input():
+    return [l.strip() for l in open("day2.in", 'r').readlines()]
+
+def p1():
+    checksum = 0
+
+    for line in get_input():
+        sorted_row = sorted(map(int, line.split()))
+        checksum += (int(sorted_row[-1]) - int(sorted_row[0]))
+
+    print(checksum)
+
+def p2():
+    checksum = 0
+
+    for line in get_input():
+        nums = line.split()
+        for i, n in enumerate(nums):
+            n = int(n)
+            for j in nums[i+1:]:
+                j = int(j)
+                if (n / j == n // j) or (j / n == j // n):
+                    if (n / j == n // j):
+                        checksum += n // j
+                    else:
+                        checksum += j // n
+        
+    print(checksum)
+
+
+print("-- Part One --")
+p1()
+print("-- Part Two --")
+p2()
